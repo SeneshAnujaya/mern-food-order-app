03a4a41e79de0edb2fd51da098b867dac07ae5e2
SeneshAnujaya <senesh9637@gmail.com>
2025-02-03 23:18:45 +0530

    create home page ui

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
-function layout() {
+type Props = {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: Props) {
   return (
-    <div>layout</div>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Hero />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
+    </div>
+  );
 }
 
-export default layout
+export default Layout;
