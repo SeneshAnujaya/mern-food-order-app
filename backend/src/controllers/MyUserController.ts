3281466af0336f66ebae679ba0e1c7b08afe034d
SeneshAnujaya <senesh9637@gmail.com>
2025-02-06 22:28:41 +0530

    create userProfile api &  validations

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from "express";
 import User from "../models/user";
+import { count } from "console";
 
-export const createCurrentUser = async (req: Request, res: Response) : Promise<void> => {
+export const createCurrentUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   try {
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
 
     if (existingUser) {
-     res.status(200).send();
-     return;
-    
+      res.status(200).send();
+      return;
     }
 
     const newUser = new User(req.body);
@@ -21,5 +24,26 @@ export const createCurrentUser = async (req: Request, res: Response) : Promise<v
   }
 };
 
+export const updateCurrentUser = async (req: Request, res: Response) : Promise<any> => {
+  try {
+    const { name, addressLine1, country, city } = req.body;
+    const user = await User.findById(req.userId);
 
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
 
+    user.name = name;
+    user.addressLine1 = addressLine1;
+    user.city = city;
+    user.country = country;
+
+    await user.save();
+
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error updating user" });
+  }
+};
--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,9 +1,12 @@
 
 import  express  from "express";
-import  { createCurrentUser } from "../controllers/MyUsercontroller";
+import  { createCurrentUser, updateCurrentUser } from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
-router.post("/", createCurrentUser);
+router.post("/", jwtCheck, createCurrentUser);
+router.put('/', jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser);
 
 export default router;
