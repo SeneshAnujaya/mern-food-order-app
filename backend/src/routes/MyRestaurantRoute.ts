2e1d37d575f011010f43bec7944bc4d3a138c008
SeneshAnujaya <senesh9637@gmail.com>
2025-02-19 01:59:59 +0530

    create update restaurant form functionality backend & frontend

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,6 @@
 import  express  from "express";
 import multer from "multer";
-import { createMyRestaurant, getMyRestaurant } from "../controllers/MyRestaurantController";
+import { createMyRestaurant, getMyRestaurant, updateMyRestaurant } from "../controllers/MyRestaurantController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyRestaurantRequest } from "../middleware/validation";
 
@@ -17,5 +17,6 @@ const upload = multer({
 // api/my/resturant
 router.get("/", jwtCheck, jwtParse, getMyRestaurant);
 router.post("/", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, createMyRestaurant)
+router.put("/", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, updateMyRestaurant)
 
 export default router;
